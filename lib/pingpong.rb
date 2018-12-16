386dfebac25701db468bd14eb7f6710627c996ba
Serhii Hopkalo <Sergey Gopkalo@gmail.com>
2018-12-16 21:19:28 +0200

    draft commit

--- lib/pingpong.rb
+++ lib/pingpong.rb
@@ -2,5 +2,37 @@ require "pingpong/version"
 
 module Pingpong
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Vernacular::AST::Modifiers
+    
+    # Extends Ruby syntax to allow build wrapper around method, as in:
+    # def_wrap original_method; end
+    class WrapInstanceMethods < ASTModifier
+      def initialize
+        super
+        
+        # Extend the parser 
+        extend_parser(:def_wrap, '')
+        
+
+        # Extend the builder by adding a 'some' function that will build
+        # wrapper around original method
+        extend_builder()
+
+        # Extend the rewriter by adding an 'def_wrap' callback, which will be
+        # called when 'def_wrap' node is added to the AST.
+        build_rewriter { include WrapInstanceMethodsRewriter }
+      end
+
+      module WrapInstanceMethodsRewriter
+        def def_wrap(method_node)
+
+          # check is there method with name _<original_method>
+          # change the original method to _<original_method>
+          # write wrapper around original method
+
+        end
+      end
+    end
+  end
 end
---
+++ lib/pingpong/modifiers/track_method.rb
@@ -0,0 +1,169 @@
+# frozen_string_literals: true
+
+require 'astrolabe/builder'
+require 'parser/current'
+require 'unparser'
+
+#############################
+# lib/vernacular/ast.rb
+# we can set the project file path to prevent other the project files to be compiled
+
+# fix TreeRewriter#rewrite method to avoid passing ast parameter
+module Parser
+  class PPTreeRewriter < TreeRewriter
+
+    def rewrite(buffer)
+      super(buffer, Parser::CurrentRuby.new.parse(buffer))
+    end
+
+  end
+end
+
+class AddPPMethod < Parser::PPTreeRewriter
+
+  # change method name to pp_<original_method_name>
+  # add wrapper method as <original_method_name>
+  def on_def(node)
+    method_name = node.children.first
+    method_name_range = node.loc.name
+
+    replace(method_name_range, "pp_#{method_name}")
+
+    rewriter      = ModifyMethodBody.new
+    buffer        = Parser::Source::Buffer.new('(example)')
+    buffer.source = Unparser.unparse(node)
+    wrap_method   = rewriter.rewrite(buffer)
+
+    insert_before(node.loc.expression, "\n")
+    insert_before(node.loc.expression, "\n")
+    insert_before(node.loc.expression, wrap_method)
+
+    super
+  end
+
+end
+
+class ModifyMethodBody < Parser::PPTreeRewriter
+
+  # change the method body to track the arguments, 
+  # call the original method, track the result
+  def on_def(node)
+    method_name = node.children.first
+    body_range = node.children[2].loc.expression
+
+    arguments = node.children[1].children.map do |arg_node|
+      arg_node.children.first
+    end.join(", ")
+    
+    method_call = ["pp_#{method_name}", "(", arguments, ")"].join
+
+    code = <<~PP.chomp
+      method(__method__).parameters.each do |_, arg|
+          puts "\#{arg}: \#{binding.local_variable_get(arg)}"
+        end
+
+        result = #{method_call}
+
+        puts result
+    PP
+
+    replace(body_range, code)
+
+    super
+  end
+
+end
+
+rewriter = AddPPMethod.new
+buffer = Parser::Source::Buffer.new('(example)')
+buffer.source = <<~RUBY
+  def test(x, y)
+    puts x
+    puts y
+  end
+RUBY
+puts rewriter.rewrite(buffer)
+
+def parse_source(source)
+  require 'astrolabe/builder'
+  require 'parser/current'
+  buffer = Parser::Source::Buffer.new('(string)')
+  buffer.source = source
+
+  ast_builder = Astrolabe::Builder.new
+  parser = Parser::CurrentRuby.new(ast_builder)
+
+  parser.parse(buffer)
+end
+
+module Vernacular
+  module Modifiers
+    # Extend Ruby syntax to match ~def...~end
+    class TrackMethod < RegexModifier
+
+      include ParseSource
+
+      def initialize
+        super(/~def\s+.+~end/m) 
+      end
+
+      # We have to parse the source, build ast, rewrite code,
+      # write back to source
+      def modify(source)
+        source.gsub(/~def/, 'def')
+        source.gsub(/~end/, 'end')
+
+        parse_source(source)
+      end
+    end
+  end
+end
+
+module ParseSource
+
+  def parse_source(source)
+    require 'astrolabe/builder'
+    require 'parser/current'
+
+    parser = Parser::CurrentRuby.new
+
+    buffer = Parser::Source::Buffer.new('(string)')
+    buffer.source = source
+
+    parser.parse(buffer)
+  end
+
+end
+
+module MethodRewriter
+
+  # Triggered wherenever a `:def` node is added to the AST.
+  # Hope nobody will use pp_[original_method_name] convention
+  # We can rise exeption in case of same name convention
+  def on_def(node)
+    method_name = node.children.first
+    original_method_name = "pp_#{method_name}"
+
+    node.children.first = original_method_name
+
+    source = <<-RUBY
+      def #{method_name}
+        output = original_method_name
+        # store output to DB
+      end
+    RUBY
+
+    parse_source(source)
+
+
+
+
+  end
+
+  private
+
+  def build_method(method_name, arg_list_node)
+
+
+  end
+end
