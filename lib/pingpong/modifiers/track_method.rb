f81345fb7801e6a826e9bbf70f25b446242bc1de
Serhii Hopkalo <Sergey Gopkalo@gmail.com>
2018-12-17 18:42:59 +0200

    Move modifiers to Parser module

--- lib/pingpong/modifiers/track_method.rb
+++ lib/pingpong/modifiers/track_method.rb
@@ -10,6 +10,7 @@ require 'unparser'
 
 # fix TreeRewriter#rewrite method to avoid passing ast parameter
 module Parser
+
   class PPTreeRewriter < TreeRewriter
 
     def rewrite(buffer)
@@ -17,64 +18,65 @@ module Parser
     end
 
   end
-end
 
-class AddPPMethod < Parser::PPTreeRewriter
+  class AddPPMethod < PPTreeRewriter
 
-  # change method name to pp_<original_method_name>
-  # add wrapper method as <original_method_name>
-  def on_def(node)
-    method_name = node.children.first
-    method_name_range = node.loc.name
+    # change method name to pp_<original_method_name>
+    # add wrapper method as <original_method_name>
+    def on_def(node)
+      method_name = node.children.first
+      method_name_range = node.loc.name
 
-    replace(method_name_range, "pp_#{method_name}")
+      replace(method_name_range, "pp_#{method_name}")
 
-    rewriter      = ModifyMethodBody.new
-    buffer        = Parser::Source::Buffer.new('(example)')
-    buffer.source = Unparser.unparse(node)
-    wrap_method   = rewriter.rewrite(buffer)
+      rewriter      = ModifyMethodBody.new
+      buffer        = Parser::Source::Buffer.new('(example)')
+      buffer.source = Unparser.unparse(node)
+      wrap_method   = rewriter.rewrite(buffer)
 
-    insert_before(node.loc.expression, "\n")
-    insert_before(node.loc.expression, "\n")
-    insert_before(node.loc.expression, wrap_method)
+      insert_before(node.loc.expression, "\n")
+      insert_before(node.loc.expression, "\n")
+      insert_before(node.loc.expression, wrap_method)
 
-    super
+      super
+    end
+
+  end
+
+  class ModifyMethodBody < PPTreeRewriter
+
+    # change the method body to track the arguments, 
+    # call the original method, track the result
+    def on_def(node)
+      method_name = node.children.first
+      body_range = node.children[2].loc.expression
+
+      arguments = node.children[1].children.map do |arg_node|
+        arg_node.children.first
+      end.join(", ")
+      
+      method_call = ["pp_#{method_name}", "(", arguments, ")"].join
+
+      code = <<~PP.chomp
+        method(__method__).parameters.each do |_, arg|
+            puts "\#{arg}: \#{binding.local_variable_get(arg)}"
+          end
+
+          result = #{method_call}
+
+          puts result
+      PP
+
+      replace(body_range, code)
+
+      super
+    end
+
   end
 
 end
 
-class ModifyMethodBody < Parser::PPTreeRewriter
-
-  # change the method body to track the arguments, 
-  # call the original method, track the result
-  def on_def(node)
-    method_name = node.children.first
-    body_range = node.children[2].loc.expression
-
-    arguments = node.children[1].children.map do |arg_node|
-      arg_node.children.first
-    end.join(", ")
-    
-    method_call = ["pp_#{method_name}", "(", arguments, ")"].join
-
-    code = <<~PP.chomp
-      method(__method__).parameters.each do |_, arg|
-          puts "\#{arg}: \#{binding.local_variable_get(arg)}"
-        end
-
-        result = #{method_call}
-
-        puts result
-    PP
-
-    replace(body_range, code)
-
-    super
-  end
-
-end
-
-rewriter = AddPPMethod.new
+rewriter = Parser::AddPPMethod.new
 buffer = Parser::Source::Buffer.new('(example)')
 buffer.source = <<~RUBY
   def test(x, y)
