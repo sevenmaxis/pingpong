b81d827cb29269f511d9a4cd464a021632e681a6
Serhii Hopkalo <Sergey Gopkalo@gmail.com>
2018-12-19 15:45:38 +0200

    Remove old aproach of tracking

--- lib/pingpong/modifiers/track_method.rb
+++ lib/pingpong/modifiers/track_method.rb
@@ -39,61 +39,6 @@ module Parser
 
   end
 
-  class AddPPMethod < PPTreeRewriter
-
-    # change method name to pp_<original_method_name>
-    # add wrapper method as <original_method_name>
-    def on_def(node)
-      method_name = node.children.first
-      method_name_range = node.loc.name
-
-      replace(method_name_range, "pp_#{method_name}")
-
-      rewriter      = ModifyMethodBody.new
-      buffer        = Parser::Source::Buffer.new('(example)')
-      buffer.source = Unparser.unparse(node)
-      wrap_method   = rewriter.rewrite(buffer)
-
-      insert_before(node.loc.expression, "\n")
-      insert_before(node.loc.expression, "\n")
-      insert_before(node.loc.expression, wrap_method)
-
-      super
-    end
-
-  end
-
-  class ModifyMethodBody < PPTreeRewriter
-
-    # change the method body to track the arguments, 
-    # call the original method, track the result
-    def on_def(node)
-      method_name = node.children.first
-      body_range = node.children[2].loc.expression
-
-      arguments = node.children[1].children.map do |arg_node|
-        arg_node.children.first
-      end.join(", ")
-      
-      method_call = ["pp_#{method_name}", "(", arguments, ")"].join
-
-      code = <<~PP.chomp
-        method(__method__).parameters.each do |_, arg|
-            puts "\#{arg}: \#{binding.local_variable_get(arg)}"
-          end
-
-          result = #{method_call}
-
-          puts result
-      PP
-
-      replace(body_range, code)
-
-      super
-    end
-
-  end
-
 end
 
 rewriter = Parser::InsertTrack.new
